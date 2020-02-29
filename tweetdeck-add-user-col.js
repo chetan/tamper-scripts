70a7a0084a178ea6a2f262b72dffbfab22acf05b
Chetan Sarva <chetan@pixelcop.net>
2020-02-29 11:46:11 -0500

    initial import: tweetdeck add user col script

---
+++ tweetdeck-add-user-col.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         Tweetdeck - Add user col
+// @namespace    https://github.com/chetan
+// @version      0.1
+// @description  Add a user column with a single click
+// @author       You
+// @match        https://tweetdeck.twitter.com/
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js
+// @include      https://tweetdeck.twitter.com/*
+// @run-at       document-end
+// ==/UserScript==
+// console = unsafeWindow.console;
+
+(function() {
+  function waitFor(sel) {
+    return new Promise((resolve, reject) => {
+      const start = performance.now();
+      const id = setInterval(() => {
+        const el = $(sel);
+        if (el.length === 0) {
+          if (performance.now() - start >= 10000) {
+            clearInterval(id);
+            reject(new Error(`unable to find ${sel} within 10sec`));
+          }
+          return;
+        }
+        // found it!
+        clearInterval(id);
+        resolve(el);
+      }, 50);
+    });
+  }
+
+  function clickAddColumn() {
+    waitFor('button.js-add-column').then((el) => {
+      el.click();
+      $('.js-modal-panel .icon-close').click();
+    });
+  }
+
+  function clickTweetsButton() {
+    waitFor('.js-modal-context .icon-twitter-bird').then((el) => {
+      el.click();
+      clickAddColumn();
+    });
+  }
+
+  function onAddClick(e) {
+    const el = $(e.target);
+    const tweet = $(el).parents('.tweet');
+    tweet
+      .find('.account-inline')
+      .first()
+      .click();
+    clickTweetsButton();
+  }
+
+  function addPlusButton() {
+    $('span.username').each(function() {
+      // const username = $(this).text();
+      const tweet = $(this).parents('.tweet');
+      if (tweet.find('.quick-plus').length > 0) {
+        return;
+      }
+
+      const icon = tweet.find('.avatar');
+      if (icon.length === 0) {
+        return;
+      }
+
+      const a = document.createElement('a');
+      a.className = 'quick-plus';
+      // a.setAttribute('data-username', username);
+      a.innerText = 'add';
+      $(a).css({
+        position: 'absolute',
+        left: '15px',
+        top: `${icon.offsetParent().position().top + 36}px`,
+      });
+      tweet.find('.tweet-body').after(a);
+      $(a).on('click', onAddClick);
+    });
+  }
+
+  function main() {
+    addPlusButton();
+  }
+
+  setInterval(() => {
+    main();
+  }, 1000);
+})();
